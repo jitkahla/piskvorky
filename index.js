310d60d17b243858a4b6109ba78ded6c637babc2
Jitka Hladká <jitka.hladka@gmail.com>
2022-04-26 18:59:57 +0200

    Fix js - mění se i popisek obrázku

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 //začíná kolečko
 let plays = 'circle';
+const playsImg = document.querySelector('.plays');
 
-//funkce co vyplní políčko po kliknutí kolečkem, znepřístupní ho a zařídí, aby se dostal na řadu křížek nebo naopak
+//funkce co vyplní políčko po kliknutí kolečkem pomocí třídy, znepřístupní ho a zařídí, aby se dostal na řadu křížek nebo naopak, mění se img src a alt
 const blockFill = (event) => {
   event.target.classList.add(`field-${plays}`);
   event.target.disabled = true;
   if (plays === 'circle') {
     plays = 'cross';
+    playsImg.alt = 'Křížek';
   } else {
     plays = 'circle';
+    playsImg.alt = 'Kolečko';
   }
-  document.querySelector('.plays').src = `images/${plays}.svg`;
+  playsImg.src = `images/${plays}.svg`;
 };
 
 //vybírám všechny buttony v hracím poli a přidávám na ně eventlistener, který kliknutím spustí fci výše
